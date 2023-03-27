c9aeab9c0ad42f576b739a1376420eefba57f1c3
Oleksandr Levytskiy <lev151975@ukr.net>
2023-03-28 01:12:52 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ingredientsItems = document.querySelector("#ingredients");
+const liElements = ingredients.map((ingredient) => {
+  const li = document.createElement("li");
+  li.classList.add("item");
+  li.textContent = ingredient;
+  return li;
+});
+ingredientsItems.append(...liElements);
+console.log(ingredientsItems);
